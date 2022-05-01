1d8b2f114340a6a69d60d37912539d8099819188
gabrielbf <gabrielbp+github@gmail.com>
2022-05-01 01:12:50 -0300

    Inclusão de select para campo estado em componente em UserForm

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -45,6 +45,12 @@ class UserForm extends React.Component {
     this.setState({
       [name]: value
     });
+
+    if (name === 'estado'){
+      this.setState({
+        uf: value
+      });
+    }
   }
 
   handleSubmit(event) {
@@ -137,7 +143,37 @@ class UserForm extends React.Component {
           <div className='row'>
             <div className='col-4'>
               <label htmlFor='estado' className='form-label'>Estado</label>
-              <input name='estado' className='form-control' type='text' value={this.state.estado} onChange={this.handleChange} />
+              <select className='form-select' id='estado' name='estado' onChange={this.handleChange}>
+                <option value=''>Selecione o Estado</option>
+                <option value='AC'>Acre</option>
+                <option value='AL'>Alagoas</option>
+                <option value='AP'>Amapá</option>
+                <option value='AM'>Amazonas</option>
+                <option value='BA'>Bahia</option>
+                <option value='CE'>Ceará</option>
+                <option value='DF'>Distrito Federal</option>
+                <option value='ES'>Espírito Santo</option>
+                <option value='GO'>Goiás</option>
+                <option value='MA'>Maranhão</option>
+                <option value='MT'>Mato Grosso</option>
+                <option value='MS'>Mato Grosso do Sul</option>
+                <option value='MG'>Minas Gerais</option>
+                <option value='PA'>Pará</option>
+                <option value='PB'>Paraíba</option>
+                <option value='PR'>Paraná</option>
+                <option value='PE'>Pernambuco</option>
+                <option value='PI'>Piauí</option>
+                <option value='RJ'>Rio de Janeiro</option>
+                <option value='RN'>Rio Grande do Norte</option>
+                <option value='RS'>Rio Grande do Sul</option>
+                <option value='RO'>Rondônia</option>
+                <option value='RR'>Roraima</option>
+                <option value='SC'>Santa Catarina</option>
+                <option value='SP'>São Paulo</option>
+                <option value='SE'>Sergipe</option>
+                <option value='TO'>Tocantins</option>
+                <option value='EX'>Estrangeiro</option>
+              </select>
             </div>
             <div className='col-2'>
               <label htmlFor='uf' className='form-label'>UF</label>
