20ac5c2599e00c29dfceef775f71795d64a9980a
gabrielbf <gabrielbp+github@gmail.com>
2022-05-01 00:31:46 -0300

    Alteração de MenuLinkSuperior para um componente de classe e lógica para desabilitar links que da tela presente

--- src/components/MenuLinksSuperior/MenuLinksSuperior.js
+++ src/components/MenuLinksSuperior/MenuLinksSuperior.js
@@ -3,24 +3,57 @@ import PropTypes from 'prop-types';
 import './MenuLinksSuperior.css';
 import { Link } from 'react-router-dom';
 
-const MenuLinksSuperior = () => (
-  <div className="MenuLinksSuperior" role="navigation">
-    <ul className="nav">
-      <li className="nav-item">
-        <a className="nav-link disabled" aria-current="Home" href="#">Home</a>
-      </li>
-      <li className="nav-item">
-        <Link className='nav-link' to="/UserAccounts">Contas</Link>
-      </li>
-      <li className="nav-item">
-        <Link className='nav-link' to="/UserForm">Usuários</Link>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Configurações</a>
-      </li>
-    </ul>
-  </div>
-);
+//const MenuLinksSuperior = () => (
+//  <div className="MenuLinksSuperior" role="navigation">
+//    <ul className="nav">
+//      <li className="nav-item">
+//        <a className="nav-link disabled" aria-current="Home" href="#">Home</a>
+//      </li>
+//      <li className="nav-item">
+//        <Link className='nav-link' to="/UserAccounts">Contas</Link>
+//      </li>
+//      <li className="nav-item">
+//        <Link className='nav-link' to="/UserForm">Usuários</Link>
+//      </li>
+//      <li className="nav-item">
+//        <a className="nav-link" href="#">Configurações</a>
+//      </li>
+//    </ul>
+//  </div>
+//);
+
+class MenuLinksSuperior extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  
+  render() {
+    return (
+      <div className="MenuLinksSuperior" role="navigation">
+        <ul className="nav">
+          <li className="nav-item">
+            {this.props.ativo === 'home'
+              ? <Link className="nav-link disabled" aria-current="Home" to='/'>Home</Link>
+              : <Link className="nav-link" to='/'>Home</Link>
+            }
+          </li>
+          <li className="nav-item">
+            <Link className='nav-link' to="/UserAccounts">Contas</Link>
+          </li>
+          <li className="nav-item">
+            {this.props.ativo === 'userForm'
+              ? <Link className='nav-link disabled' aria-current="UserForms" to="/UserForm">Usuários</Link>
+              : <Link className='nav-link' to="/UserForm">Usuários</Link>
+            }
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Configurações</a>
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
 
 MenuLinksSuperior.propTypes = {};
 
