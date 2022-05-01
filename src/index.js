d9709af60d5cc340031c4519fdb2555ab0d457d1
gabrielbf <gabrielbp+github@gmail.com>
2022-05-01 02:51:32 -0300

    Inclusão de rota para componente UserAccounts em index.js

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ root.render(
     <Router>
       <Routes>
         <Route path="/UserForm" element={<UserForm />} />
+        <Route path="/UserAccounts" element={<UserAccounts />} />
         <Route path="/" element={<AdminHome />} />
       </Routes>
     </Router>
