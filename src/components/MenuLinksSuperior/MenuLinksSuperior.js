c212ae99b7a161fbdb7237f23bee720b76b9d1af
gabrielbf <gabrielbp+github@gmail.com>
2022-05-01 05:08:06 -0300

    Troca de áspas duplas por simples em componente MenuLinksSuperior

--- src/components/MenuLinksSuperior/MenuLinksSuperior.js
+++ src/components/MenuLinksSuperior/MenuLinksSuperior.js
@@ -3,25 +3,6 @@ import PropTypes from 'prop-types';
 import './MenuLinksSuperior.css';
 import { Link } from 'react-router-dom';
 
-//const MenuLinksSuperior = () => (
-//  <div className="MenuLinksSuperior" role="navigation">
-//    <ul className="nav">
-//      <li className="nav-item">
-//        <a className="nav-link disabled" aria-current="Home" href="#">Home</a>
-//      </li>
-//      <li className="nav-item">
-//        <Link className='nav-link' to="/UserAccounts">Contas</Link>
-//      </li>
-//      <li className="nav-item">
-//        <Link className='nav-link' to="/UserForm">Usuários</Link>
-//      </li>
-//      <li className="nav-item">
-//        <a className="nav-link" href="#">Configurações</a>
-//      </li>
-//    </ul>
-//  </div>
-//);
-
 class MenuLinksSuperior extends React.Component {
   constructor(props) {
     super(props);
@@ -29,25 +10,28 @@ class MenuLinksSuperior extends React.Component {
   
   render() {
     return (
-      <div className="MenuLinksSuperior" role="navigation">
-        <ul className="nav">
-          <li className="nav-item">
+      <div className='MenuLinksSuperior' role='navigation'>
+        <ul className='nav'>
+          <li className='nav-item'>
             {this.props.ativo === 'home'
-              ? <Link className="nav-link disabled" aria-current="Home" to='/'>Home</Link>
-              : <Link className="nav-link" to='/'>Home</Link>
+              ? <Link className='nav-link disabled' aria-current='Home' to='/'>Home</Link>
+              : <Link className='nav-link' to='/'>Home</Link>
             }
           </li>
-          <li className="nav-item">
-            <Link className='nav-link' to="/UserAccounts">Contas</Link>
+          <li className='nav-item'>
+            {this.props.ativo === 'userAccounts'
+              ? <Link className='nav-link disabled' aria-current='UserAccounts' to='/UserAccounts'>Contas</Link>
+              : <Link className='nav-link' to='/UserAccounts'>Contas</Link>
+            }
           </li>
-          <li className="nav-item">
+          <li className='nav-item'>
             {this.props.ativo === 'userForm'
-              ? <Link className='nav-link disabled' aria-current="UserForms" to="/UserForm">Usuários</Link>
-              : <Link className='nav-link' to="/UserForm">Usuários</Link>
+              ? <Link className='nav-link disabled' aria-current='UserForms' to='/UserForm'>Usuários</Link>
+              : <Link className='nav-link' to='/UserForm'>Usuários</Link>
             }
           </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Configurações</a>
+          <li className='nav-item'>
+            <a className='nav-link' href='#'>Configurações</a>
           </li>
         </ul>
       </div>
