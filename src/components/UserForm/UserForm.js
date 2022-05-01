295c7714995625f1fbffe3ecd6b50c89cfa2a933
gabrielbf <gabrielbp+github@gmail.com>
2022-04-30 21:23:07 -0300

    Começo de lógica para componente UserForm

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -12,7 +12,10 @@ import MenuLinksSuperior from '../MenuLinksSuperior/MenuLinksSuperior';
 class UserForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {
+      nome: '',
+      email: ''
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -20,23 +23,39 @@ class UserForm extends React.Component {
 
   handleChange(event) {
     // TODO Validação aqui
-    this.setState({value: event.target.value});
+    const target = event.target;
+    //const value = target.type === 'checkbox' ? target.checked : target.value;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+    //this.setState({email: event.target.email})
   }
 
   handleSubmit(event) {
-    alert('Nome substituido: ' + this.state.value);
+    alert('Nome substituido: ' + this.state.nome + '\n' +
+    'E-Mail substituido: ' + this.state.email
+    );
     event.preventDefault();
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <div className='container'>
+        <form onSubmit={this.handleSubmit}>
+          <div className='mb-3'>
+            <label htmlFor='nome' className='form-label'>Nome</label>
+            <input name='nome' className='form-control' type='text' value={this.state.nome} onChange={this.handleChange} />
+          </div>
+          <div className='mb-3'>
+            <label htmlFor='email' className='form-label'>E-Mail</label>
+            <input name='email' className='form-control' type='email' value={this.state.email} onChange={this.handleChange} />
+          </div>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     );
   }
 }
